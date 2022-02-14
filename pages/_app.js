0d21de0b920db0a7240dc7e1e85da845a45686b4
qbxt <q@queue.bot>
2022-02-14 13:30:26 -0600

    add mouse gestures

--- pages/_app.js
+++ pages/_app.js
@@ -17,7 +17,7 @@ function MyApp({ Component, pageProps }) {
     }, []);
 
     return (
-        <ThemeProvider>
+        <ThemeProvider enableSystem={false}>
             <Head>
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fork-awesome@1.2.0/css/fork-awesome.min.css" integrity="sha256-XoaMnoYC5TH6/+ihMEnospgm0J1PM/nioxbOUdnM8HY=" crossOrigin="anonymous"></link>
                 <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet"></link>
